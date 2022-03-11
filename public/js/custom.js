7bc35db5029d8e780c9f4ee0b269dc57a64f4774
Rashid Hussain <rashid.bukhari78600@gmail.com>
2022-03-11 11:27:44 +0500

    ww-31 ajax setup to prefill eod

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,11 +1,14 @@
-$.ajax({
-    url: '/test',
-    type: 'get',
-    data: {'q':'', '_token': $('input[name=_token]').val()},
-    success: function(data){
-        console.log(data);
-    },
-    error: function(data){
-        // Not found
-    }
-});
+function getEodContent() {
+    $.ajax({
+        url: '/test',
+        type: 'get',
+        data: {'q':'', '_token': $('input[name=_token]').val()},
+        success: function(data){
+            console.log(data);
+        },
+        error: function(data){
+            // Not found
+        }
+    });
+}
+getEodContent();
