dc50afc7318c3d25be785c7f280771ff94206c4f
Rashid Hussain <rashid.bukhari78600@gmail.com>
2022-03-16 11:29:23 +0500

    ww-31 update api to get current targets that have today tasks

--- app/Http/Controllers/Voyager/EodController.php
+++ app/Http/Controllers/Voyager/EodController.php
@@ -5,16 +5,42 @@ namespace App\Http\Controllers\Voyager;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use App\Models\ProjectTarget;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
-class EodController extends Controller
+/**
+ * Eod -> End of Day Email Controller
+ */
+class EodController extends VoyagerBaseController
 {
-    public function eodContent() {
-        $project = Project::with(['targets' => function ($q) {
-                            $q->whereDate('created_at', today());
-                            $q->whereDeveloperId(Auth::user()->id);
-                        }, 'targets.tasks', 'eodConfiguration'])->first();
+    /**
+     * Get Today Target tasks with configuration
+     * to send in email
+     * @return JsonResponse
+     */
+    public function eodContent(): JsonResponse
+    {
+        $project = Project::with(['targets' => function ($targetQry) {
+                                $targetQry->whereDeveloperId(Auth::user()->id);
+                                $targetQry->whereHas('tasks', function (Builder $targetTaskQry) {
+                                $targetTaskQry->whereDate('created_at', today());
+                                });
+                            }, 'eodConfiguration', 'targets.tasks'])->first();
         return response()->json(['data'=> $project]);
+    }
+
+    /**
+     * Override store method to send email before store
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+            dd($request);
+//            parent::store();
     }
 }
