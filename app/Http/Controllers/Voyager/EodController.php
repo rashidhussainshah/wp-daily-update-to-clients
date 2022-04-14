1626523b10193710b725709222c8388ea307979f
Rashid Hussain <rashid.bukhari78600@gmail.com>
2022-04-15 01:15:25 +0500

    ww-31 lang validation messages

--- app/Http/Controllers/Voyager/EodController.php
+++ app/Http/Controllers/Voyager/EodController.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Controllers\Voyager;
 
-use App\Http\Controllers\Controller;
 use App\Mail\EndOfDayReport;
-use App\Mail\EodMail;
 use App\Models\Project;
-use App\Models\ProjectTarget;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
-use TCG\Voyager\Facades\Voyager;
 
 /**
  * Eod -> End of Day Email Controller
@@ -31,9 +24,7 @@ class EodController extends \TCG\Voyager\Http\Controllers\VoyagerBaseController
     {
         $project = Project::with(['targets' => function ($targetQry) {
                                 $targetQry->whereDeveloperId(Auth::user()->id);
-                                $targetQry->whereHas('tasks', function (Builder $targetTaskQry) {
-                                $targetTaskQry->whereDate('created_at', today());
-                                });
+                                $targetQry->whereHas('tasks');
                             }, 'eodConfiguration' => function($eodConfQry) {
                                 $eodConfQry->whereDeveloperId(Auth::user()->id);
                                 }, 'targets.tasks'])->first();
@@ -47,7 +38,24 @@ class EodController extends \TCG\Voyager\Http\Controllers\VoyagerBaseController
      */
     public function store(Request $request)
     {
+
         $project = Project::find($request->project_id);
+        $vError = false;
+        if (!isset($project->eodConfiguration)) {
+                $vErrorMessage = __('eod.configuration_not_found');
+                $vError = true;
+        }
+        elseif (!isset($project->eodConfiguration->client)) {
+            $vErrorMessage = __('eod.client_not_found');
+                $vError = true;
+        }
+        if ($vError) {
+            return redirect()->back()->with([
+                'message'    => $vErrorMessage,
+                'alert-type' => 'error',
+            ]);
+        }
+        dd($vError);
         Mail::to($project->eodConfiguration->client->email)
             ->cc(explode(',', $project->eodConfiguration->cc))
             ->bcc(explode(',', $project->eodConfiguration->bcc))
