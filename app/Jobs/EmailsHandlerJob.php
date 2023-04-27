7906888d6b8715af42f91c4bb3319904d7f4cd24
Rashid Hussain <rashid.bukhari78600@gmail.com>
2023-04-27 08:20:43 +0500

    WP-56 slack integrated

--- app/Jobs/EmailsHandlerJob.php
+++ app/Jobs/EmailsHandlerJob.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
+use Spatie\SlackAlerts\Facades\SlackAlert;
 
 class EmailsHandlerJob implements ShouldQueue
 {
@@ -50,11 +51,11 @@ class EmailsHandlerJob implements ShouldQueue
                         \Mail::to($this->data['to'])->bcc(explode(',', $this->data['bcc']))
                             ->send($mail);
                     } else {
-                        \Mail::to($this->data['to'])
-                            ->send($mail);
+                        \Mail::to($this->data['to'])->send($mail);
                     }
                     if ($this->data['enable_slack']) {
-                        Log::channel('slackEODNotificationLog')->info(strip_tags($this->data['dynamic_eod_content']));
+//                        Log::channel('slackEODNotificationLog')->info(strip_tags($this->data['dynamic_eod_content']));
+                        SlackAlert::to($this->data['slack_webhook_url'])->message(strip_tags($this->data['dynamic_eod_content']));
                     }
                     break;
                 //============== Default ==============\\
