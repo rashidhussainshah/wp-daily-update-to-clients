6463d04dfb6369c7c2aedc8f43e4d30f67dfdaf0
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2023-07-01 16:37:45 +0500

    fee module

---
+++ database/migrations/2023_07_01_163257_create_student_fees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentFeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_fees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id');
+            $table->double('amount');
+            $table->string('status')->default('pending');
+            $table->foreignId('receiver_id');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_fees');
+    }
+}
