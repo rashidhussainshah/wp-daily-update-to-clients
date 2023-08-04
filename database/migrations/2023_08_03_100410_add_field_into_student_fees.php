a0a000065f98e1dbcdab279595742291f70d4142
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2023-08-04 23:34:02 +0500

    add fields

--- database/migrations/2023_08_03_100410_add_field_into_student_fees.php
+++ database/migrations/2023_08_03_100410_add_field_into_student_fees.php
@@ -15,6 +15,7 @@ class AddFieldIntoStudentFees extends Migration
     {
         Schema::table('student_fees', function (Blueprint $table) {
             $table->string('batch')->after('id');
+
             $table->dateTime('date')->nullable()->after('batch');
         });
     }
