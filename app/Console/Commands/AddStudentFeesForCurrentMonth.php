0a3e7a6b4fc57d38b1ca1d9882826096a33df84e
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2023-11-03 20:52:29 +0500

    add command for students fee

---
+++ app/Console/Commands/AddStudentFeesForCurrentMonth.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\StudentFee;
+use App\Models\User;
+use Carbon\Carbon;
+
+class AddStudentFeesForCurrentMonth extends Command
+{
+    protected $signature = 'add:fees';
+    protected $description = 'Add student fees for the current month';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        // Get the current month and year
+        $currentMonth = Carbon::now()->format('F');
+        $currentYear = Carbon::now()->year;
+
+        // Fetch users with role_id equal to 12 (adjust the role_id as needed)
+        $students = User::where('role_id', 12)->get();
+
+        foreach ($students as $student) {
+            // Replace 'your_batch_name' with the appropriate batch name
+            $batchName = 'HTML CSS JS PHP';
+
+            // Check if a fee record for the current month and year already exists
+            $existingFee = StudentFee::where('student_id', $student->id)
+                ->whereMonth('date', '=', Carbon::now()->month)
+                ->whereYear('date', '=', $currentYear)
+                ->first();
+
+            if (!$existingFee) {
+                // Create a new StudentFee record for the current student
+                StudentFee::create([
+                    'batch' => $batchName,
+                    'date' => Carbon::now(),
+                    'student_id' => $student->id,
+                    'receiver_id' => 1,
+                    'amount' => 2000,
+                    'status' => 'pending',
+                    // You can set other fields as needed
+                ]);
+            } else {
+                // Update the existing fee record
+//                $existingFee->update([
+//                    'amount' => 2000,
+//                    'status' => 'pending',
+//                    // You can set other fields as needed
+//                ]);
+            }
+        }
+
+        $this->info("Fees updated for all students for the current month.");
+    }
+}
