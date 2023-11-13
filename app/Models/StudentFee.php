e6a7c1884287b3ee8c70bdbb680b8910bcc5b68b
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2023-11-13 21:31:48 +0500

    Update StudentFee.php

--- app/Models/StudentFee.php
+++ app/Models/StudentFee.php
@@ -11,23 +11,30 @@ class StudentFee extends Model
     use HasFactory, SoftDeletes;
     protected $fillable = ['batch', 'date', 'student_id', 'receiver_id', 'amount', 'status'];
     // Scope to get data for the current month
+    // Scope to get data for the current month
     public function scopeCurrentMonth($query)
     {
         return $query->whereMonth('date', now()->month);
     }
 
-    // Scope to get data where status is pending
-    public function scopePendingStatus($query)
+    // Scope to get data where status is Paid
+    public function scopePaidStatus($query)
     {
-        return $query->where('status', 'pending');
+        return $query->where('status', 'Paid');
     }
 
-    // Combined scope to get data for the current month and where status is pending
-    // Combined scope to get data for the current month or where status is pending
+    // Scope to get data where status is Pending
+    public function scopePendingStatus($query)
+    {
+        return $query->where('status', 'Pending');
+    }
+
+    // Combined scope to get data for the current month with Paid status or where status is Pending
     public function scopeCurrentMonthOrPendingStatus($query)
     {
         return $query->where(function ($query) {
             $query->currentMonth()
+                ->paidStatus()
                 ->orWhere(function ($query) {
                     $query->pendingStatus();
                 });
