1051351bb6045cdd58206cfff16798ecb598abee
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2023-11-13 21:20:25 +0500

    Update StudentFee.php

--- app/Models/StudentFee.php
+++ app/Models/StudentFee.php
@@ -10,4 +10,21 @@ class StudentFee extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = ['batch', 'date', 'student_id', 'receiver_id', 'amount', 'status'];
+    // Scope to get data for the current month
+    public function scopeCurrentMonth($query)
+    {
+        return $query->whereMonth('date', now()->month);
+    }
+
+    // Scope to get data where status is pending
+    public function scopePendingStatus($query)
+    {
+        return $query->where('status', 'pending');
+    }
+
+    // Combined scope to get data for the current month and where status is pending
+    public function scopeCurrentMonthAndPendingStatus($query)
+    {
+        return $query->currentMonth()->pendingStatus();
+    }
 }
