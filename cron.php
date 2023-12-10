860a82eea29b213d6ca4ddeeee3c9ee9217c9e8b
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2023-12-10 18:33:29 +0500

    cron setup

---
+++ cron.php
@@ -0,0 +1,13 @@
+<?php
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/vendor/autoload.php';
+
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+$kernel->bootstrap();
+
+$kernel->call('schedule:run');
