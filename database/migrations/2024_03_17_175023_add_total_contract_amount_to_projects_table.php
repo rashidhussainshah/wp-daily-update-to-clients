031ed9674abcf828b6047060898d11bb40306018
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2024-03-17 17:56:17 +0500

    total contract amount

--- database/migrations/2024_03_17_175023_add_total_contract_amount_to_projects_table.php
+++ database/migrations/2024_03_17_175023_add_total_contract_amount_to_projects_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class AddTotalContractAmountToProjectsTable extends Migration
@@ -14,8 +15,11 @@ class AddTotalContractAmountToProjectsTable extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->string('total_contract_amount')->after('payment_mode')->nullable();
+            $table->string('total_contract_amount')->nullable()->after('payment_mode');
         });
+
+        // Modify the default value for expected_delivery_date
+        DB::statement("ALTER TABLE projects MODIFY expected_delivery_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL");
     }
 
     /**
