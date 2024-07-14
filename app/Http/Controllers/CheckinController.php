8bf6bf01db04298ee5315bc06a6bc48b436d7e50
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2024-07-14 23:24:36 +0500

    fix 1 hours short issue

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -12,6 +12,7 @@ class CheckinController extends Controller
 {
     public function storeCheckin(Request $request)
     {
+        date_default_timezone_set('Asia/Karachi');
         Checkin::create([
             'developer_id' => Auth::id(),
             'checkin_at' => now(),
@@ -32,6 +33,7 @@ class CheckinController extends Controller
                 ],
             ],
         ];
+        dd($blocks);
 
         // Send the message to Slack using blocks
         SlackAlert::blocks($blocks);
@@ -43,16 +45,16 @@ class CheckinController extends Controller
         $checkin = Checkin::where('developer_id', Auth::id())
             ->whereNull('checkout_at')
             ->whereDate('checkin_at', Carbon::today())
-            ->first();
+            ->latest()->first();
 
         if (!$checkin) {
             return redirect()->back()->with('error', 'No check-in information found for today. Please check-in first.');
         }
 
-        $checkin->update([
-            'checkout_at' => now(),
-            'end_of_day_report' => $request->input('end_of_day_report'),
-        ]);
+//        $checkin->update([
+//            'checkout_at' => now(),
+//            'end_of_day_report' => $request->input('end_of_day_report'),
+//        ]);
 
         $user = Auth::user();
         $endOfDayReport = $request->input('end_of_day_report');
@@ -62,20 +64,22 @@ class CheckinController extends Controller
         $totalOfficeHours = $user->part_time ? 4.5 : 9;
         $checkinAt = Carbon::parse($checkin->checkin_at);
         $checkoutAt = Carbon::parse($checkin->checkout_at);
-
         $hoursSpent = $checkinAt->diffInHours($checkoutAt);
 
         // Calculate shortfall in hours
         $hoursShort = $totalOfficeHours - $hoursSpent;
+        dd($checkinAt, $checkoutAt, $totalOfficeHours, $hoursShort);
 
         // Add short time with message if hours are insufficient
         $message = "*Check-out done for " . $user->name . " at " . $currentTime . "*\n\n*EOD Report:*\n" . $endOfDayReport . "\n\n";
         if ($hoursShort > 0) {
             $minutesSpent = $checkinAt->diffInMinutes($checkoutAt) % 60;
             $minutesShort = 60 - $minutesSpent;
+            $hoursShort = $hoursShort - 1;
 
             $message .= "*Short by " . "*" . $hoursShort . " hours and " . $minutesShort . " minutes.*";
         }
+        dd($message);
 
         $blocks = [
             [
@@ -91,4 +95,5 @@ class CheckinController extends Controller
         return redirect()->back()->with('success', 'Check-out message sent to Slack!');
 
         return redirect()->back()->with('success', 'Checked out successfully!');
-    }}
+    }
+}
