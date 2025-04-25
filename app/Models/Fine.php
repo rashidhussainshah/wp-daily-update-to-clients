0eece64b37eeb7ba12c925e3675f6ecdff962011
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2025-04-25 23:40:21 +0500

    Update Fine.php

--- app/Models/Fine.php
+++ app/Models/Fine.php
@@ -5,9 +5,18 @@ namespace App\Models;
 use App\utils\traits\CommonRelationship;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Fine extends Model
 {
     use HasFactory, CommonRelationship;
     protected $fillable = ['amount', 'user_id', 'reason', 'date', 'note'];
+    public function scopeCurrentUserAndManagement($query)
+    {
+        if (Auth::user()->role && (Auth::user()->role->name == USER::ADMINISTRATOR_ROLE_NAME || Auth::user()->role->name == USER::ACCOUNTANT_ROLE_NAME)) {
+            return $query->orderBy('created_at', 'desc');
+        } else {
+            return $query->where('user_id', Auth::user()->id)->orderBy('created_at', 'desc');
+        }
+    }
 }
