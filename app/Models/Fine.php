50194cf8fa897316fae8161447e6e3dd018aef8f
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2025-04-25 23:34:10 +0500

    Update Fine.php

--- app/Models/Fine.php
+++ app/Models/Fine.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\utils\traits\CommonRelationship;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fine extends Model
 {
-    use HasFactory;
+    use HasFactory, CommonRelationship;
     protected $fillable = ['amount', 'user_id', 'reason', 'date', 'note'];
 }
