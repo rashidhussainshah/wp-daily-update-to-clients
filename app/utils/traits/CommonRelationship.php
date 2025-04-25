69f3101c758ddc8950a5952d2bdb302a196e5609
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2025-04-25 23:37:22 +0500

    Update CommonRelationship.php

--- app/utils/traits/CommonRelationship.php
+++ app/utils/traits/CommonRelationship.php
@@ -14,4 +14,13 @@ trait CommonRelationship
     {
         return $query->where('developer_id', Auth::user()->id);
     }
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function scopeCurrentUserByUserId($query)
+    {
+        return $query->where('user_id', Auth::user()->id);
+    }
 }
