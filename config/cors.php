2f9b8ffeebd43f0bdc2dbde152cdc3d4f4e17b9b
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2025-12-06 12:05:39 +0500

    Update allowed origins in CORS config
    Added 'https://staging.webpenter.com' and 'http://localhost:3000' to the list of allowed origins to support additional environments.

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://webpenter.com', 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://houzilo.com'],
+    'allowed_origins' => ['https://webpenter.com', 'https://staging.webpenter.com', 'http://localhost:3000', 'http://127.0.0.1:5173', 'https://houzilo.com'],
 
     'allowed_origins_patterns' => [],
 
