af33ab4b079a040022d3102be0313cb9e16bbcb7
Rashid Bukhari <rashid.bukhari78600@gmail.com>
2026-01-11 21:14:04 +0500

    change time

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -57,7 +57,7 @@ class CheckinController extends Controller
             // Apply fine only if slot is enabled + dev team member
             if ($lateCheckinFineEnabled && $isDevTeam) {
                 // Use user-defined time or default to 09:50
-                $checkinTime = $user->checkin_time ?? '09:52';
+                $checkinTime = $user->checkin_time ?? '10:17';
 
                 $allowedTime = Carbon::parse($todayDate . ' ' . $checkinTime);
 
